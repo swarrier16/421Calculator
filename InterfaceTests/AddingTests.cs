84182b80c57ed6030d99a1c572ea2a105e8d0723
swarrier16 <swarrier16@gmail.com>
2020-02-11 09:47:42 -0800

    added updated interface tests

---
+++ InterfaceTests/AddingTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Addition;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addition.Tests
+{
+    [TestClass()]
+    public class AddingTests
+    {
+        Adding Addition = new Adding();
+        private readonly int aInt = 1;
+        private readonly int bInt = 2;
+        private readonly double aDouble = 1.5;
+        private readonly double bDouble = 2.5;
+        private readonly decimal aDecimal = 1.5M;
+        private readonly decimal bDecimal = 2.5M;
+
+        [TestMethod()]
+        public void SumTest()
+        {
+            Addition.Sum(aInt, bInt);
+            Assert.AreEqual(3, Adding.Result);
+        }
+
+       /* [TestMethod()]
+        public void SumDoubleTest()
+        {
+            Addition.Sum(aDouble, bDouble);
+            Assert.AreEqual(4, Adding.Result);
+        }*/
+
+        [TestMethod()]
+        public void SumTest1()
+        {
+            decimal[] arrayList = { 1.1M, 2.1M, 3.1M };
+            Addition.Sum(arrayList);
+            Assert.AreEqual(6.3M, Adding.Result);
+        }
+    }
+}
---
+++ InterfaceTests/SquaringTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Square;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Square.Tests
+{
+    [TestClass()]
+    public class SquaringTests
+    {
+        Squaring Square = new Squaring();
+        private readonly int aInt = 1;
+        private readonly int bInt = 2;
+        private readonly double aDouble = 1.5;
+        private readonly double bDouble = 2.5;
+        private readonly decimal aDecimal = 1.5M;
+        private readonly decimal bDecimal = 2.5M;
+
+        [TestMethod()]
+        public void PowerDoubleTest()
+        {
+            Square.Power(aDouble, bDouble);
+            Assert.AreEqual(2.7556759606310752, Squaring.Result);
+        }
+
+        [TestMethod()]
+        public void PowerIntTest()
+        {
+            Square.Power(aInt, bInt);
+            Assert.AreEqual(1, Squaring.Result);
+        }
+    }
+}
