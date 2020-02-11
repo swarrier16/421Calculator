ea57867ddce4c2510927a0e40c4d76d4c1587d9d
swarrier16 <swarrier16@gmail.com>
2020-02-10 16:52:27 -0800

    added square root interface

---
+++ SquareRoot/SquareRooting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MathOperations;
+
+namespace SquareRoot
+{
+    public interface IRoot
+    {
+        public dynamic Root(dynamic a);
+    }
+    public class SquareRooting : IRoot { 
+    
+        private static dynamic _result;
+
+        public static dynamic Result
+        {
+            get => _result;
+            set => _result = value;
+        }
+        public static void Main()
+        {
+        }
+        public dynamic Root(dynamic a)
+    {
+        Result = MathOperations.Roots.Root(a);
+        return Result;
+    }
+}
+}
