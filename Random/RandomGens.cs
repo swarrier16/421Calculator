9b7d3b3d58be173768221afa2d57e4ef34b5d4b5
swarrier16 <swarrier16@gmail.com>
2020-02-27 21:02:30 -0800

    completed random functions

--- Random/RandomGens.cs
+++ Random/RandomGens.cs
@@ -1,43 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RandomGen
 {
     public class Randos
     {
-        public static int Generate(int seed)
+        public static int GenerateInt(int rangeMin, int rangeMax)
         {
-            var _random = new Random(seed);
-            int _randomNum = _random.Next();
-            return _randomNum;
-        }
-
-        public static int Generate(int seed, int rangeMin, int rangeMax)
-        {
-            var _random = new Random(seed);
+            var _random = new Random();
             int _randomNum = _random.Next(rangeMin, rangeMax);
             return _randomNum;
         }
 
-        public static int[] Generate(int seed, int size)
+        public static decimal GenerateDecimal(decimal rangeMin, decimal rangeMax)
         {
-            var _random = new Random(seed);
-            int[] result = new int[size];
+            var _random = new Random();
+            decimal _randomNum = ((decimal)_random.NextDouble() * (rangeMax - rangeMin) + rangeMin);
+            _randomNum = (decimal)Math.Round(_randomNum, 2);
 
-            for (int i = 0; i < size; i++)
-            {
-                result[i] = _random.Next();
-            }
-
-            return result;
+            return _randomNum;
         }
 
-        public static decimal Generate(decimal seed)
+        public static int GenerateList(int seed, int rangeMin, int rangeMax)
         {
-            var _random = new Random((int)seed);
-
-            decimal _randomNum = (decimal)_random.NextDouble();
-            _randomNum = (decimal)Math.Round(_randomNum, 1);
-
+            var _random = new Random(seed);
+            int _randomNum = _random.Next(rangeMin, rangeMax);
             return _randomNum;
         }
 
@@ -51,7 +38,22 @@ namespace RandomGen
             return _randomNum;
         }
 
-        public static decimal[] Generate(decimal seed, int size)
+        public static int[] GenerateList(int seed, int size, int rangeMin, int rangeMax)
+        {
+            var _random = new Random(seed);
+            int[] result = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = _random.Next(rangeMin, rangeMax);
+                
+            }
+
+            return result;
+        }
+
+
+        public static decimal[] Generate(decimal seed, int size, decimal rangeMin, decimal rangeMax)
         {
             var _random = new Random((int)seed);
             decimal _randomNum;
@@ -59,30 +61,69 @@ namespace RandomGen
 
             for (int i = 0; i < size; i++)
             {
-                _randomNum = (decimal)_random.NextDouble();
+                _randomNum = ((decimal)_random.NextDouble() * (rangeMax - rangeMin) + rangeMin);
                 _randomNum = (decimal)Math.Round(_randomNum, 2);
 
                 result[i] = _randomNum;
+              
             }
-
-            Console.WriteLine(result[4]);
 
             return result;
         }
 
+        public static decimal RandomSelect(decimal seed, int size, int rangeMin, int rangeMax)
+        {
+            var _random = new Random();
+            decimal[] result = new decimal[size];
+            result = Generate(seed, size, rangeMin, rangeMax);
+            int index = _random.Next(size);
+            return (result[index]);
+        }
+
+        public static decimal SeedSelect(decimal seed, int size, int rangeMin, int rangeMax)
+        {
+            var _random = new Random((int)seed);
+            decimal[] result = new decimal[size];
+            result = Generate(seed, size, rangeMin, rangeMax);
+            int index = _random.Next(size);
+            return (result[index]);
+        }
+
+        public static decimal[] NSelect(decimal seed, int size, int rangeMin, int rangeMax, int resultSize)
+        {
+            var _random = new Random();
+            decimal[] result = new decimal[size];
+            decimal[] new_result = new decimal[resultSize];
+            int index;
+            result = Generate(seed, size, rangeMin, rangeMax);
+
+            for (int i = 0; i < resultSize; i++)
+            {
+                index = _random.Next(size);
+                new_result[i] = result[index];
+            }
+
+         
+
+            return (new_result);
+        }
+
+        public static decimal[] NSeedSelect(decimal seed, int size, int rangeMin, int rangeMax, int resultSize)
+        {
+            decimal[] result = new decimal[size];
+            result = Generate(seed, size, rangeMin, rangeMax);
+            var _random = new Random((int)seed);
+            int index;
+            decimal[] new_result = new decimal[resultSize];
+            
+            for (int i = 0; i < resultSize; i++)
+            {
+                index = _random.Next(size);
+                new_result[i] = result[index];
+            }
+
+            return (new_result);
+        }
+
     }
 }
-
-
-/*#5
- *
- * If selection of:
- * 0
- * 1
- * 2
- * 3
- *
- * Random function with seed 5 and range of 0-3
- *
- * Then select the value from the list
- */
